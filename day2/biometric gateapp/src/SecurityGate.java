1bd9b768711d3c3612a4f71c77ccbbef2f7b7103
varshini48 <varshiniakireddy@gmail.com>
2026-02-03 13:22:36 +0530

    add security logic to the code

---
+++ day2/biometric gateapp/src/SecurityGate.java
@@ -0,0 +1,16 @@
+public class SecurityGate {
+    public static void main(String[] args){
+        String passengerName="Amelia Barner";
+        int passengerAge=25;
+        double passengerBaggageWeight=18.5;
+        boolean passengerHasBoardingPass=true;
+        char gender ='F';
+
+        System.out.println("Processing Passenge: "+ passengerName);
+        System.out.println("checking baggage weight" + passengerBaggageWeight+ "kg");
+        System.out.println("passenger: " + passengerName +" is allowed to board to the plane: " + passengerHasBoardingPass);
+        
+
+
+    }
+}
